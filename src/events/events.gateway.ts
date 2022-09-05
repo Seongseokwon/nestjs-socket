bcb38452bfb797c761c3473bf1bb6c6613bc7ecb
sseong1 <sseong@deepnoid.com>
2022-09-05 14:19:18 +0900

    소켓 서버 연결

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -6,24 +6,38 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { Logger } from "@nestjs/common";
 
-@WebSocketGateway({ namespace: '//ws-.+/' })
+@WebSocketGateway({
+  cors : {
+    origin: ['http://localhost:3000']
+  }
+})
 export class EventsGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
   @WebSocketServer() public server: Server;
+  private logger: Logger = new Logger('Event Gateway');
 
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  handleMessage(client: Socket, payload: string): any {
+    console.log(client.data);
+    console.log(client.rooms);
+    console.log(payload);
+    return this.server.emit('toClient', 'hello');
   }
 
   afterInit(server: Server): any {
     console.log('websocket server init');
   }
 
-  handleConnection(client: any, ...args: any[]): any {}
+  handleConnection(client: any, ...args: any[]): any {
+    console.log('@@', client.id);
+    this.logger.log(`Client connected: ${client.id}`);
+  }
 
-  handleDisconnect(client: any): any {}
+  handleDisconnect(client: any): any {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
 }
